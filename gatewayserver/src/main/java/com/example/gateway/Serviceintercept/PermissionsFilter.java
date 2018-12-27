4c5f1a756454a8a2bf1711a32d9d54fc70a0ee1b
lxqqiqi <45193472+lxqqiqi@users.noreply.github.com>
2018-12-27 15:32:43 +0800

    Add files via upload

--- gatewayserver/src/main/java/com/example/gateway/Serviceintercept/PermissionsFilter.java
+++ gatewayserver/src/main/java/com/example/gateway/Serviceintercept/PermissionsFilter.java
@@ -4,11 +4,9 @@ package com.example.gateway.Serviceintercept;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Component
 public class PermissionsFilter extends ZuulFilter {
@@ -30,23 +28,7 @@ public class PermissionsFilter extends ZuulFilter {
         HttpServletRequest request = requestContext.getRequest();
 //在控制台打印访问的路径
         System.out.println(request.getRequestURI());
-        TypeCode typeCode = new TypeCode();
-
-        int type = typeCode.getType();
-        if(type==0) {
-            return false;
-        } else {
-
-            //需要权限校验URL
-            if ("/balance/hello/hello/test".equalsIgnoreCase(request.getRequestURI())) {
-                return true;
-//        } else if ("/apigateway/order/api/v1/order/list".equalsIgnoreCase(request.getRequestURI())) {
-//            return true;
-//        } else if ("/apigateway/order/api/v1/order/find".equalsIgnoreCase(request.getRequestURI())) {
-//            return true;
-            }
-            return false;
-        }
+return false;
     }
 
     @Override
@@ -55,15 +37,15 @@ public class PermissionsFilter extends ZuulFilter {
 //        HttpServletRequest request = context.getRequest();
 //        String name = request.getHeader("name");
 
-        int a = 0;
-        if(a<1) {
-            //false代表的意思是,这个请求最终不会被zuul转发到后端服务器
-            context.setSendZuulResponse(false);
-            context.setResponseStatusCode(401);
-            try {
-                context.getResponse().getWriter().write("Does not have the permission");
-            } catch (Exception e) {}
-        }
+//        int a = 0;
+//        if(a<1) {
+//            //false代表的意思是,这个请求最终不会被zuul转发到后端服务器
+//            context.setSendZuulResponse(false);
+//            context.setResponseStatusCode(401);
+//            try {
+//                context.getResponse().getWriter().write("Does not have the permission");
+//            } catch (Exception e) {}
+//        }
         return null;
     }
 }
